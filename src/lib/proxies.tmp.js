6b0dbe1ad28947f4a3c915698270865823aaba9c
Nafish Ahmed <nafish.ahmed.dev@gmail.com>
2023-04-30 19:51:22 +0530

    dynamic as module

--- src/lib/proxies.tmp.js
+++ src/lib/proxies.tmp.js
@@ -1,1 +1,13 @@
-module.exports = [{}];
+module.exports = [
+    {
+        host: "*",
+        prefix: "/api/v1/mynu",
+        target: "http://localhost:8000/auth",
+        ws: false,
+        interceptors: [
+            (req, res, next) => {
+                return next();
+            }
+        ]
+    },
+]
